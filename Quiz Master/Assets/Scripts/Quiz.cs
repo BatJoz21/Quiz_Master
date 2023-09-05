112340045f0225af87689b0d8a8abefb807a1c95
Jozka Siregar <jozkasiregar@gmail.com>
2023-09-05 15:32:28 +0700

    setting up answer

--- Quiz Master/Assets/Scripts/Quiz.cs
+++ Quiz Master/Assets/Scripts/Quiz.cs
@@ -7,9 +7,16 @@ public class Quiz : MonoBehaviour
 {
     [SerializeField] QuestionSO questionSO;
     [SerializeField] TextMeshProUGUI questionText;
+    [SerializeField] GameObject[] answerButtons;
 
     void Start()
     {
         questionText.text = questionSO.GetQuestion();
+
+        for(int i = 0; i<answerButtons.Length; i++)
+        {
+            TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+            buttonText.text = questionSO.GetAnswer(i);
+        }
     }
 }
